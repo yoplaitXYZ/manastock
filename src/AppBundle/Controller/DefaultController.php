9577d69b96e5509b1568cf5884c3ab1573564642
rash <rachiokiller@gmail.com>
2017-07-16 18:28:09 +0200

    commit projet2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace AppBundle\Controller;
-
+use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,11 +11,44 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+     public function indexAction(Request $request)
+     {
+         $em = $this->getDoctrine()->getManager();
+         $produits = $em->getRepository('AppBundle:Produit')->findAll();
+         return $this->render('default/index.html.twig', [
+             'produits' => $produits
+         ]);
+
+     }
+     /**
+   * @Route("/produit/add", name="produit_add")
+   */
+  public function addProduitAction(Request $request)
+  {
     }
+        /**
+      * @Route("/produit/edit/{id}", name="produit_edit")
+      */
+      public function editproduitAction(Request $request,$id)
+      {
+
+      }
+      /**
+      * @Route("/produit/supp/{id}", name="produit_supp")
+      */
+      public function supproduitAction(Request $request,$id)
+      {
+
+              $em = $this->getDoctrine()->getManager();
+              $produits = $em->getRepository('AppBundle:Produit')->find($id);
+
+              if (null === $produits) {
+                  echo "produit n'a pas etait trouvé";
+                  die;
+              }
+
+              $em->remove($produits);
+              $em->flush();
+              return $this->redirectToRoute('homepage');
+      }
 }
